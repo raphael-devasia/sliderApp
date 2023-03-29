701a91780c3d22d875f9b6c9dd51caa06c5c6f87
raphael-devasia <122820743+raphael-devasia@users.noreply.github.com>
2023-03-29 13:19:22 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Carousel from './Carousel';
+import SlickCarousal from './SlickCarousel';
+
+
+const App = () => {
+  return (
+    <main>
+      <SlickCarousal/>
+    </main>
+  );
+};
+export default App;
